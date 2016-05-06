eb3b03f67a578aecf702b6033931c757e253edd8
Mathias-Rui Lopes <lopes.mathiasrui@gmail.com>
2016-05-06 11:24:59 +0200

    Adding the plugin file

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+Plugin Name: paypalESGI-plugin
+Description: Plugin wordpress crée par les étudiants ESGI: Mathias-Rui Lopes, Alexandre Lau, Thomas Gauret, Benjamin Montbailly
+Plugin URI: http://www.esgi.fr/ecole-informatique.html
+Tags: PayPal payment, PayPal, button, payment, online payments, pay now, buy now, ecommerce, gateway, paypal button, paypal buy now button, paypal plugin
+Author: Mathias-Rui Lopes
+Author URI: http://mrlopes.fr
+License: GPL2
+Version: 0.1
+*/
+
+
+?>
